6b841557702b6c31fb6c8fa31a47c24b0875f6cd
Ole Magnus Fon Johnsen <me@omfj.no>
2023-04-18 23:21:07 +0200

    Add favicons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { SessionProvider } from "next-auth/react";
 import { api } from "@/utils/api";
 import { Toaster } from "@/components/ui/toast";
 import "@/styles/globals.css";
+import Head from "next/head";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -11,6 +12,9 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <Toaster />
       <Component {...pageProps} />
     </SessionProvider>
